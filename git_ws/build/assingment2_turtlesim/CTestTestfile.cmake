69dca0ccd0bc04b0e01c3fa3bdeb6036b45ad890
jkcolem <jkcolem@g.clemson.edu>
2021-02-04 00:48:37 -0500

    workspace

---
+++ git_ws/build/assingment2_turtlesim/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/jkcolem/AuE893_JohnColeman_Sp21/ros_ws/src/assingment2_turtlesim
+# Build directory: /home/jkcolem/AuE893_JohnColeman_Sp21/ros_ws/build/assingment2_turtlesim
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
