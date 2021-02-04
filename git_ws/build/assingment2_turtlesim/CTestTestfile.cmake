31b600971aa3494ddda9856d47a01d7bab8fe04d
jkcolem <jkcolem@g.clemson.edu>
2021-02-04 00:51:26 -0500

    workspace

--- git_ws/build/assingment2_turtlesim/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jkcolem/AuE893_JohnColeman_Sp21/ros_ws/src/assingment2_turtlesim
-# Build directory: /home/jkcolem/AuE893_JohnColeman_Sp21/ros_ws/build/assingment2_turtlesim
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- git_ws/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/jkcolem/AuE893_JohnColeman_Sp21/ros_ws/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/jkcolem/AuE893_JohnColeman_Sp21/ros_ws/devel;/opt/ros/noetic'.split(';') if '/home/jkcolem/AuE893_JohnColeman_Sp21/ros_ws/devel;/opt/ros/noetic' != '' else []
