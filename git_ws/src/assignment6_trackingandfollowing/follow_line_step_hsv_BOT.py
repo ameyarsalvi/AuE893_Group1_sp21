7ffeded3c0e588eeb6d54b8032bc8e63a930d7f3
jkcolem <60300096+jkcolem@users.noreply.github.com>
2021-03-07 20:38:16 -0500

    Add files via upload

---
+++ git_ws/src/assignment6_trackingandfollowing/follow_line_step_hsv_BOT.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python3
+import rospy
+import cv2
+import numpy as np
+from cv_bridge import CvBridge, CvBridgeError
+from geometry_msgs.msg import Twist
+from sensor_msgs.msg import Image
+from move_robot import MoveTurtlebot3
+
+class LineFollower(object):
+
+    def __init__(self):
+        self.bridge_object = CvBridge()
+        self.image_sub = rospy.Subscriber("/raspicam_node/image",Image,self.camera_callback)
+        self.moveTurtlebot3_object = MoveTurtlebot3()
+
+    def camera_callback(self, data):
+        print("HERE")
+        # We select bgr8 because its the OpneCV encoding by default
+        cv_image = self.bridge_object.imgmsg_to_cv2(data, desired_encoding="bgr8")
+
+        # We get image dimensions and crop the parts of the image we dont need
+        height, width, channels = cv_image.shape
+        #crop_img = cv_image[int((height/2)+100):int((height/2)+120)][1:int(width)]
+        crop_img = cv_image[255:][200:int(width-200)]
+        #crop_img = cv_image[340:360][1:640]
+
+        # Convert from RGB to HSV
+        hsv = cv2.cvtColor(crop_img, cv2.COLOR_BGR2HSV)
+        # Define the Yellow Colour in HSV
+
+        """
+        To know which color to track in HSV use ColorZilla to get the color registered by the camera in BGR and convert to HSV. 
+        """
+
+        # Threshold the HSV image to get only yellow colors
+        s=70
+        lower_yellow = np.array([0,0,255-s])
+        upper_yellow = np.array([255,s,255])
+        mask = cv2.inRange(hsv, lower_yellow, upper_yellow)
+
+        # Calculate centroid of the blob of binary image using ImageMoments
+        m = cv2.moments(mask, False)
+
+        try:
+            cx, cy = m['m10']/m['m00'], m['m01']/m['m00']
+        except ZeroDivisionError:
+            cx, cy =  width/2, height/2
+        
+        # Draw the centroid in the resultut image
+        # cv2.circle(img, center, radius, color[, thickness[, lineType[, shift]]]) 
+        cv2.circle(mask,(int(cx), int(cy)), 10,(0,0,255),-1)
+        cv2.imshow("Original", crop_img)
+        cv2.imshow("MASK", mask)
+        cv2.waitKey(1)
+
+        #################################
+        ###   ENTER CONTROLLER HERE   ###
+        #################################
+
+        pub_= rospy.Publisher('/cmd_vel', Twist, queue_size=1)
+        msg=Twist()
+        k = 0.001/1.5
+        angular_z = -k*(cx-width/2)
+        linear_x = 0.1
+
+        thresh = 1.5
+        if angular_z > thresh:
+            angular_z = thresh
+        elif angular_z < -thresh:
+            angular_z = -thresh
+        print("Errors:")
+        print([cx,cy,width/2,height/2])
+        print("Controls")
+        print([angular_z,linear_x])
+        msg.linear.x = linear_x
+        msg.angular.z = angular_z
+        pub_.publish(msg)
+
+        rospy.loginfo("ANGULAR VALUE SENT===>"+str(msg.angular.z))
+        # Make it start turning
+        self.moveTurtlebot3_object.move_robot(msg)
+        
+
+    def clean_up(self):
+        self.moveTurtlebot3_object.clean_class()
+        cv2.destroyAllWindows()
+
+def main():
+    rospy.init_node('line_following_node', anonymous=True)
+    line_follower_object = LineFollower()
+    rate = rospy.Rate(5)
+    
+    ctrl_c = False
+    def shutdownhook():
+        # works better than the rospy.is_shut_down()
+        line_follower_object.clean_up()
+        rospy.loginfo("shutdown time!")
+        ctrl_c = True
+        pub_= rospy.Publisher('/cmd_vel', Twist, queue_size=1)
+        msg=Twist()
+        msg.linear.x = 0
+        msg.angular.z = 0
+        pub_.publish(msg)
+    
+    rospy.on_shutdown(shutdownhook)
+    
+    rospy.spin()
+
+if __name__ == '__main__':
+        main()
+        
